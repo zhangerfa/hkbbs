f9c94e2616f1163a0360202e8d0c94d372fd1ec8
erfa_zhang <877319363@qq.com>
2022-12-31 22:18:14 +0800

    添加一个过滤器

--- src/main/Java/config/SpringMvcConfig.java
+++ src/main/Java/config/SpringMvcConfig.java
@@ -1,11 +1,31 @@
 package config;
 
+import controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@ComponentScan({"controller", "config"})
+@ComponentScan("controller")
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        // 将自定义拦截器添加到SpringMVC容器中，并设置拦截路径,可以使用通配符
+        registry.addInterceptor(projectInterceptor).
+                addPathPatterns("/users", "users/*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 当访问/html/ 路径下资源时访问/html/目录下的内容(而不是被springmvc处理)
+        registry.addResourceHandler("/html/**").addResourceLocations("/html/");
+        // 同理，设置其他静态资源
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
 }
