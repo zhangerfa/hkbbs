e7b5dabfac816bc3971eb625167d0b24c928a6cc
erfa <877319363@qq.com>
2023-01-01 12:25:39 +0800

    首页资源跳转改为重定向，请求转发会使url地址不变，但html中的<a ref>进行资源跳转时根据当前url获取前缀来拿到新资源的url,，会导致访问出错

--- src/main/Java/controller/HomeController.java
+++ src/main/Java/controller/HomeController.java
@@ -3,11 +3,13 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 // 实现访问首页时资源跳转
 @Controller
 public class HomeController {
     @RequestMapping
     public String homeForward(){
-        return "forward:/html/wall.html";
+        return "redirect:/html/wall.html";
     }
 }
