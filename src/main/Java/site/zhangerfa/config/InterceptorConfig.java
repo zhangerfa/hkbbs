d77acf83d5dc48376257c92c7ef79b4331fe5d3d
erfa_zhang <877319363@qq.com>
2023-01-05 16:19:36 +0800

    删除前端模板中自带的logo

--- src/main/Java/site/zhangerfa/config/InterceptorConfig.java
+++ src/main/Java/site/zhangerfa/config/InterceptorConfig.java
@@ -1,30 +1,24 @@
 package site.zhangerfa.config;
 
 import jakarta.annotation.Resource;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.zhangerfa.controller.interceptor.CheckLoginInterceptor;
 
-@Component
-public class InterceptorConfig extends WebMvcConfigurationSupport {
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
     @Resource
     private CheckLoginInterceptor checkLoginInterceptor;
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 设置放行路径
+        String[] excludePath = {"/**/*.css", "/**/*.png", "/**/*.jpg",
+                                "/**/*.svg", "/**/*.js"};
         registry.addInterceptor(checkLoginInterceptor)
-                .addPathPatterns("/")
-                .excludePathPatterns("/static/**");
-    }
-
-    // 自定义配置类覆盖了SpringBoot默认配置类
-    // 必须重新将静态资源请求拦截到static路径下
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        super.addResourceHandlers(registry);
-
+                // 不写拦截路径默认拦截所有请求
+//                .addPathPatterns("/")
+                .excludePathPatterns(excludePath);
     }
 }
