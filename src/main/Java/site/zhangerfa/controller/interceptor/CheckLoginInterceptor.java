54f8fb8dcef414308444ed86ec7d8f379f43c68c
erfa_zhang <877319363@qq.com>
2023-01-05 17:28:34 +0800

    当登陆凭证有效时将用户数据放入会话共享空间hostHandler中

--- src/main/Java/site/zhangerfa/controller/interceptor/CheckLoginInterceptor.java
+++ src/main/Java/site/zhangerfa/controller/interceptor/CheckLoginInterceptor.java
@@ -1,10 +1,15 @@
 package site.zhangerfa.controller.interceptor;
 
 import jakarta.annotation.Resource;
-import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import site.zhangerfa.pojo.User;
 import site.zhangerfa.service.UserService;
+import site.zhangerfa.util.CookieUtil;
+import site.zhangerfa.util.HostHolder;
 
 // 该拦截器用于检测访问资源时用户是否登录
 @Component
@@ -12,66 +17,64 @@ public class CheckLoginInterceptor implements HandlerInterceptor {
     @Resource
     private UserService userService;
 
+    @Resource
+    private HostHolder hostHolder;
+
     // 当用户访问登录、注册页面时如果没有携带登录凭证码放行，
     //              如果携带了则跳转到卡片墙
     // 其他页面通过检测登录凭证判断是否放行
+    //      如果放行，携带用户学号信息
     @Override
     public boolean preHandle(jakarta.servlet.http.HttpServletRequest request,
                              jakarta.servlet.http.HttpServletResponse response,
                              Object handler) throws Exception {
+        String ticket = CookieUtil.getValue(request, "ticket");
+        if (userService.checkTicket(ticket)){
+            // ticket有效，将用户信息存储到hostHolder中，供这次请求共享
+            hostHolder.setUser(userService.getUserByTicket(ticket));
+        }
 
+        // 判断是否访问与注册、登录有关页面
         String url = request.getRequestURL().toString();
         String[] pass = {"users", "login", "register"};
-
         for (String s : pass) {
             if (url.contains(s)){
                 // 访问与注册、登录有关请求，放行
-                Cookie[] cookies = request.getCookies();
-                // 检查cookie中包含有效的登录验证码
-                if (cookieContainTicket(cookies)) {
-                    // 包含有效登录验证码 跳转到卡片墙
+                if (userService.checkTicket(ticket)){
+                    // ticket有效，跳转到卡片墙
                     response.sendRedirect("/wall");
                     return false;
+                }else {
+                    // ticket无效，放行
+                    return true;
                 }
-                // 没有登录验证码或没有有效登录验证码
-                // 当前访问与注册、登录有关请求，放行
-                return true;
             }
         }
 
         // 访问 非注册、登录有关请求
-        // 检查cookie中包含有效的登录验证码
-        Cookie[] cookies = request.getCookies();
-        // 检查cookie中包含有效的登录验证码
-        if (cookieContainTicket(cookies)) {
-            // 包含有效登录验证码，放行
+        if (userService.checkTicket(ticket)){
+            // ticket有效，放行
             return true;
-        }
-
-        // 没有登陆凭证、有登录凭证已过期均跳转到登录页面
-        response.sendRedirect("/login");
-        return false;
-    }
-
-    /**
-     * 判断cookie中是否包含有效的ticket
-     *
-     */
-    private boolean cookieContainTicket(Cookie[] cookies){
-        // 未携带任何cookie
-        if (cookies == null) {
+        }else {
+            // ticket无效，跳转到登录页面
+            response.sendRedirect("/login");
             return false;
         }
-        // cookie中是否有登录验证码
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("ticket")){
-                // 判断登录验证码是否有效
-                if (userService.checkTicket(cookie.getValue())){
-                    return true;
-                }
-                return false;
-            }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // 当hostHolder中有用户数据且后端要调用模板引擎时，将数据传入modelAndView供模板引擎调用
+        User user = hostHolder.getUser();
+        if (user != null && modelAndView != null){
+            // 后端调用模板引擎且登录凭证有效
+            modelAndView.addObject("user", user);
         }
-        return false;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 当次请求清空hostHolder中请求删除
+        hostHolder.clear();
     }
 }
