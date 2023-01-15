bd6024292da42999f491bb92241cb52595d204c4
erfa_zhang <877319363@qq.com>
2023-01-16 00:31:40 +0800

    控制层增加树洞展示

--- src/main/Java/site/zhangerfa/controller/HoleController.java
+++ src/main/Java/site/zhangerfa/controller/HoleController.java
@@ -9,16 +9,20 @@ import site.zhangerfa.controller.tool.Code;
 import site.zhangerfa.controller.tool.Result;
 import site.zhangerfa.pojo.Hole;
 import site.zhangerfa.service.HoleService;
+import site.zhangerfa.util.HostHolder;
 
 @Controller
 @RequestMapping("/holes")
 public class HoleController {
     @Resource
     private HoleService holeService;
+    @Resource
+    private HostHolder hostHolder;
 
     @PostMapping
     @ResponseBody
     public Result addHole(Hole hole){
+        hole.setPosterId(hostHolder.getUser().getStuId());
         holeService.addHole(hole);
         return new Result(Code.SAVE_OK, null, "发布成功");
     }
