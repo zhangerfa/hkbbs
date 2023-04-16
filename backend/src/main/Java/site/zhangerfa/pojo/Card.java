f0e6b5dbbaa9c31c22677d83f6a7a5f4ea8e2ae2
201 <877319363@qq.com>
2023-04-16 10:21:39 +0800

    refactor 修改createTime字段名称

--- backend/src/main/Java/site/zhangerfa/pojo/Card.java
+++ backend/src/main/Java/site/zhangerfa/pojo/Card.java
@@ -3,7 +3,6 @@ package site.zhangerfa.pojo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -16,10 +15,8 @@ public class Card {
     @Schema(description = "期望的TA:描述期望中的理想征友对象")
     @NotBlank
     private String expected;
-    @Schema(description = "年级")
-    String age;
     @Schema(description = "发布时间")
-    Date create_time;
+    Date createTime;
     @Schema(description = "图片url集合")
     private List<String> imageUrls;
     @Schema(description = "交友目标")
@@ -33,24 +30,6 @@ public class Card {
     public Card(String aboutMe, String expected) {
         this.aboutMe = aboutMe;
         this.expected = expected;
-    }
-
-    /**
-     * 由学号推出年级（如果本科毕业两年称为大六）
-     * @param user
-     */
-    public void setAge(User user){
-        StringBuilder age = new StringBuilder();
-        String stuId = user.getStuId();
-        switch (stuId.charAt(0)) {
-            case 'U' -> age.append("大");
-            case 'M' -> age.append("研");
-            case 'D' -> age.append("博");
-        }
-        // 目前是上学的第几年
-        int years = Calendar.getInstance().get(Calendar.YEAR) - Integer.parseInt(stuId.substring(1, 5));
-        age.append(String.valueOf(years));
-        this.age = age.toString();
     }
 
     public String getAboutMe() {
@@ -85,20 +64,12 @@ public class Card {
         this.id = id;
     }
 
-    public Date getCreate_time() {
-        return create_time;
+    public Date getCreateTime() {
+        return createTime;
     }
 
-    public void setCreate_time(Date create_time) {
-        this.create_time = create_time;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
     }
 
     public int getGoal() {
