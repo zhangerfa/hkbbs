681e1cebadc42f6e8c19e90e244e418c42cb970e
201 <877319363@qq.com>
2023-04-16 11:33:53 +0800

    refactor page中只用来存储数据，剥离数据处理代码

--- backend/src/main/Java/site/zhangerfa/pojo/Page.java
+++ backend/src/main/Java/site/zhangerfa/pojo/Page.java
@@ -1,6 +1,5 @@
 package site.zhangerfa.pojo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "记录分页信息，并封装当前页的数据")
@@ -10,91 +9,46 @@ public class Page {
     @Schema(description = "每页大小")
     private int pageSize = 10;
     @Schema(description = "当前页上的帖子数")
-    private int numOfPostsOnPage;
+    private int numOfPostsOnCurrentPage;
     @Schema(description = "总页数")
-    private int totalPage;
-    @Schema(description = "总帖子数")
-    private int numOfPosts = -1;
+    private int numOfPages;
 
-    public Page(int currentPage, int pageSize){
-        if (pageSize < 1) throw new RuntimeException("每页显示帖子数量至少为1");
+    public Page(int currentPage, int pageSize, int numOfPages, int numOfPostsOnCurrentPage){
         this.currentPage = currentPage;
         this.pageSize = pageSize;
-    }
-
-    /**
-     * 补全page的信息
-     */
-    public void completePage(int numOfPosts){
-        this.numOfPosts = numOfPosts;
-
-        totalPage = numOfPosts / pageSize;
-        totalPage = (numOfPosts % pageSize) == 0? totalPage: totalPage + 1;
-
-        if (currentPage < totalPage)  numOfPostsOnPage = pageSize;
-        else if (totalPage == 0) numOfPostsOnPage = 0;
-        else numOfPostsOnPage = numOfPosts - (totalPage - 1) * pageSize;
-    }
-
-    /**
-     * 返回当前页首行，在使用 getOffset前需要设置 总的帖子数
-     * @return
-     */
-    @JsonIgnore
-    public int getOffset(){
-        if (numOfPosts == -1) throw new RuntimeException("未设置总帖子数，无法计算 当前页的offset");
-        return Math.min((currentPage - 1) * pageSize, totalPage);
-    }
-
-    /**
-     * 返回当前页最后一行
-     * @return
-     */
-    @JsonIgnore
-    public int getLimit(){
-        return getOffset() + pageSize + 1;
-    }
-
-    /**
-     * 获取当前总的页数
-     * @return
-     */
-    public int getTotalPage() {
-        return totalPage;
-    }
-
-    public int getNumOfPosts() {
-        return numOfPosts;
-    }
-
-    /**
-     * 获取起始页码，当前页的前两页，不会小于 1
-     * @return
-     */
-    @JsonIgnore
-    public int getFrom(){
-        return Math.max(currentPage - 2, 1);
-    }
-
-    /**
-     * 获取终止页码，当前页后两页，不会大于最大页数
-     * @return
-     */
-    @JsonIgnore
-    public int getTo() {
-        int total = getTotalPage();
-        return Math.min(currentPage + 2, total);
+        this.numOfPages = numOfPages;
+        this.numOfPostsOnCurrentPage = numOfPostsOnCurrentPage;
     }
 
     public int getCurrentPage() {
         return currentPage;
     }
 
-    public int getNumOfPostsOnPage() {
-        return numOfPostsOnPage;
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
     }
 
     public int getPageSize() {
         return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getNumOfPostsOnCurrentPage() {
+        return numOfPostsOnCurrentPage;
+    }
+
+    public void setNumOfPostsOnCurrentPage(int numOfPostsOnCurrentPage) {
+        this.numOfPostsOnCurrentPage = numOfPostsOnCurrentPage;
+    }
+
+    public int getNumOfPages() {
+        return numOfPages;
+    }
+
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
     }
 }
