136ae811a9d66f03cd81ae2d46b77e5a436e8119
201 <877319363@qq.com>
2023-04-16 10:22:45 +0800

    feat 添加由学号获得年纪的方法

--- backend/src/main/Java/site/zhangerfa/controller/tool/CardInfo.java
+++ backend/src/main/Java/site/zhangerfa/controller/tool/CardInfo.java
@@ -4,12 +4,33 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import site.zhangerfa.pojo.Card;
 import site.zhangerfa.pojo.User;
 
+import java.util.Calendar;
+
 public class CardInfo extends Card {
     @Schema(title = "发布者信息")
     User poster;
+    @Schema(description = "年级")
+    String age;
 
     public CardInfo(String aboutMe, String expect, int goal) {
         super(aboutMe, expect, goal);
+    }
+
+    /**
+     * 由学号推出年级（如果本科毕业两年称为大六）
+     */
+    public void getAge(){
+        StringBuilder age = new StringBuilder();
+        String stuId = poster.getStuId();
+        switch (stuId.charAt(0)) {
+            case 'U' -> age.append("大");
+            case 'M' -> age.append("研");
+            case 'D' -> age.append("博");
+        }
+        // 目前是上学的第几年
+        int years = Calendar.getInstance().get(Calendar.YEAR) - Integer.parseInt(stuId.substring(1, 5));
+        age.append(years);
+        this.age = age.toString();
     }
 
     public User getPoster() {
